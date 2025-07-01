2a5f23f2344cbdde98e72b237fec1c6fcfcf7879
KARTHICKKUMAR S <veerakarthick235@gmail.com>
2025-07-01 15:07:20 +0530

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Resume(models.Model):
-    name = models.CharField(max_length=100)
-    file = models.FileField(upload_to='resumes/')
-    skills = models.TextField(blank=True)
-    experience = models.TextField(blank=True)
-    parsed_data = models.JSONField(null=True, blank=True)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
